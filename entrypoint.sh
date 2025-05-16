b525c87c9b265d1ff9ab048211ad23321186f981
MerradOklom <43864503+MerradOklom@users.noreply.github.com>
2025-05-16 10:00:43 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,31 @@ cat << EOF > config.json
       "email": "${EMAIL_3:-EMAIL_3}",
       "password": "${PASSWORD_3:-PASSWORD_3}",
       "token": "${TOKEN_3:-TOKEN_3}"
+    },
+    {
+      "email": "${EMAIL_4:-EMAIL_4}",
+      "password": "${PASSWORD_4:-PASSWORD_4}",
+      "token": "${TOKEN_4:-TOKEN_4}"
+    },
+    {
+      "email": "${EMAIL_5:-EMAIL_5}",
+      "password": "${PASSWORD_5:-PASSWORD_5}",
+      "token": "${TOKEN_5:-TOKEN_5}"
+    },
+    {
+      "email": "${EMAIL_6:-EMAIL_6}",
+      "password": "${PASSWORD_6:-PASSWORD_6}",
+      "token": "${TOKEN_6:-TOKEN_6}"
+    },
+    {
+      "email": "${EMAIL_7:-EMAIL_7}",
+      "password": "${PASSWORD_7:-PASSWORD_7}",
+      "token": "${TOKEN_7:-TOKEN_7}"
+    },
+    {
+      "email": "${EMAIL_8:-EMAIL_8}",
+      "password": "${PASSWORD_8:-PASSWORD_8}",
+      "token": "${TOKEN_8:-TOKEN_8}"
     }
   ]
 }
